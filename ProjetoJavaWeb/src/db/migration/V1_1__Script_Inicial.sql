1cda0c3f76e80e4799253ba56ca03371f3fd0ce7
Iuri Menin <iurimenin@gmail.com>
2017-05-26 09:24:44 -0300

    Update V1_1__Script_Inicial.sql
    Adicionado 2 produtos padrão

--- ProjetoJavaWeb/src/db/migration/V1_1__Script_Inicial.sql
+++ ProjetoJavaWeb/src/db/migration/V1_1__Script_Inicial.sql
@@ -6,3 +6,6 @@ CREATE TABLE produtos
     descricao character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT produtos_pkey PRIMARY KEY (codigo)
 );
+
+insert into produtos(idcategoria, nome, descricao) values(0, 'Computador', 'i7');
+insert into produtos(idcategoria, nome, descricao) values(2, 'Casa', '2 andares');
